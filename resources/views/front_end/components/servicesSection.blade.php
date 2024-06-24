e71bd9a9426e0640524cbffc617be2e5a7684786
chandan <chandan84yadav@gmail.com>
2024-06-24 12:38:25 +0530

    images fix

--- resources/views/front_end/components/servicesSection.blade.php
+++ resources/views/front_end/components/servicesSection.blade.php
@@ -16,7 +16,7 @@
 
     <div class="bg-white rounded-3xl px-[20px] py-[30px] flex flex-col items-center"
          style="box-shadow: 0px 0px 10px 1px #eeb21ca8;">
-        <img src="{{asset('asset/images/Car.png')}}" alt="">
+        <img src="{{asset('storage/'.$service->image)}}" alt="">
         <h2 class="uppercase lg:text-[35px] md:text-[30px] text-center sm:text-[25px] text-[20px] text-[#15AEF1] font-bold">
             {{$service->title}}</h2>
         <p class="text-black lg:leading-7 md:leading-2 lg:text-[16px] md:text-[13px] text-[12px] font-medium font-[roboto] text-center"
