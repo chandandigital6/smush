9afa0f90f19f3dc4b2200382c1bc0bbcb9057449
rvgsystem2 <rvgsystem2@gmail.com>
2025-01-06 23:04:23 +0530

    all form changes done

--- resources/views/front_end/index.blade.php
+++ resources/views/front_end/index.blade.php
@@ -45,7 +45,7 @@
         @include('front_end.components.carSection')
 
 
-        <section class="bg-gray-50 py-12 px-6">
+        <section class="bg-white py-12 px-6">
             <div class="container mx-auto text-center">
                 <h2 class="text-3xl sm:text-4xl font-bold text-gray-800 mb-6">Welcome to MobileSmashRepair</h2>
                 <p class="text-lg text-gray-600 mb-8">
